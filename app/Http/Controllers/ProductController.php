bbb8138eaf06a57b941ad5321567864415404828
Fernando Balzan <fernando98081@gmail.com>
2020-07-20 10:30:10 -0500

    feat:create product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,9 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Product;
+use App\Category;
 
 class ProductController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index(Request $request)
     {
         if ($request) {
@@ -29,5 +35,38 @@ class ProductController extends Controller
     
         return view('products.index', compact('articulos')); 
     }
-    
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $category = Category::all();
+        return view('products.create', compact('category'));
+    }
+    /**
+      * Store a newly created resource in storage.
+      *
+      * @param  \Illuminate\Http\Request  $request
+      * @return \Illuminate\Http\Response
+      */
+      public function store (Request $request)
+      {
+          if($request->hasFile('image')){
+              $file = $request->file('image');
+              $name = time().$file->getClientOriginalName();
+              $file->move(public_path().'/images', $name);
+          }
+          $product = new product();
+          $product->name = request('name');
+          $product->category_id = $request->input('category_id');
+          $product->price = request('price');
+          $product->stock = request('stock');
+          $product->description = request('description');
+          $product->image = $name;
+          $product->save();
+          return redirect('/product');
+  
+      }
 }
