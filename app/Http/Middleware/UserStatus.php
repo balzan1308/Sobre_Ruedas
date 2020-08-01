fb636da04d8d36076861668d27c7f54627acda1a
Fernando Balzan <fernando98081@gmail.com>
2020-08-01 15:59:03 -0500

    fix:return data in controller

--- app/Http/Middleware/UserStatus.php
+++ app/Http/Middleware/UserStatus.php
@@ -4,7 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\Auth;
-use phpDocumentor\Reflection\Types\Void_;
+
 
 class UserStatus
 {
@@ -15,7 +15,7 @@ class UserStatus
      * @param Closure $next
      * @return void
      */
-    public function handle($request, Closure $next): Void_
+    public function handle($request, Closure $next)
     {
         
         $user = Auth::user();
