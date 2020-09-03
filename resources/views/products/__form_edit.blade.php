a203b5021cfab08799b49a92cc3f9e7024df71da
Fernando Balzan <fernando98081@gmail.com>
2020-09-03 15:51:42 -0500

    refactor:view create and edit of product crud

--- resources/views/products/__form_edit.blade.php
+++ resources/views/products/__form_edit.blade.php
@@ -1,21 +1,57 @@
-<div class="form-group">
-    <label for="name">name</label>
-    <input type="text" class="form-control" name="name" value="{{$product->name}}" placeholder="escribe tu nombre">
+<div class="form-group row">
+    <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('name') }}</label>
+
+    <div class="col-md-6">
+        <input id="name" type="text" class="form-control @error('name') is-invalid @enderror"value="{{$product->name}}" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+        @error('name')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
+    </div>
 </div>
-    <div class="form-group">
-    <label for="price">price</label>
-    <input type="text" class="form-control" name="price" value="{{$product->price}}" placeholder="escribe tu nombre">
+<div class="form-group row">
+    <label for="price" class="col-md-4 col-form-label text-md-right">{{ __('price') }}</label>
+
+    <div class="col-md-6">
+        <input id="price" type="text" class="form-control @error('price') is-invalid @enderror"value="{{($product->price)}}" name="price" value="{{ old('price') }}" required autocomplete="price" autofocus>
+
+        @error('price')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
+    </div>
+</div>
+<div class="form-group row">
+    <label for="stock" class="col-md-4 col-form-label text-md-right">{{ __('stock') }}</label>
+
+    <div class="col-md-6">
+        <input id="stock" type="text" class="form-control  @error('stock') is-invalid @enderror"value="{{$product->stock}}" name="stock" value="{{ old('stock') }}" required autocomplete="stock" autofocus>
+
+        @error('stock')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
+    </div>
+</div>
+<div class="form-group row">
+    <label for="description" class="col-md-4 col-form-label text-md-right">{{ __('description') }}</label>
+
+    <div class="col-md-6">
+        <input id="description" type="text" class="form-control @error('description') is-invalid @enderror"value="{{$product->description}}" name="description" value="{{ old('description') }}" required autocomplete="description" autofocus>
+
+        @error('description')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
+    </div>
 </div>
 <div class="form-group">
-       <label for="stock">stock</label>
-       <input type="text" class="form-control" name="stock" value="{{$product->stock}}" placeholder="escribe tu nombre">
-</div>
-<div class="form-group">
-      <label for="description">description</label>
-      <input type="text" class="form-control" name="description" value="{{$product->description}}" placeholder="escribe tu nombre">
-</div>
-<div class="form-group">
-    <label for="">Edith Category</label><br>
+    <label for="" class="col-md-4 col-form-label text-md-right">Edith Category</label><br>
     <select name="category_id" id="inputCategoria:id" class=·form-control>
         <option value="{{$product->category_id}}">
     @foreach($category as $categories)
@@ -33,7 +69,7 @@
 </div>
 <div class="form-group">
 
-    <label for="" >image</label>
+    <label for="" class="col-md-4 col-form-label text-md-right" >image</label>
     <input type="file" name="image" class="form-control" value="{{$product->image}}">
    
 </div>
@@ -41,7 +77,7 @@
     <label for="" >current image</label>
     <img src="../../../images/{{$product->image}}" width="200" alt="">
 </p>
-<div class="custom-control custom-checkbox">
+<div  class="custom-control custom-checkbox"  >
        <input type="checkbox" class="custom-control-input" 
         value="{{$product->active == 1 ?  1 : 0}}" 
        {{ $product->active == 1  ?  'checked' :" "}}  id="customCheck" name="active">
--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
 <div class="container">
+    <h2>Create Product <br></h2>
     <div class="row">
         <div class="col-sm-4">
 
